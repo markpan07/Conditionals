b75718de0f62cc5bd5e3b03a23d36c4667fffbc6
МАРК <markpan07@mail.ru>
2022-12-26 16:58:15 +0300

    2 task is done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         task1();
         System.out.println();
+        task2();
+        System.out.println();
+        //task3();
+        System.out.println();
     }
 
     public static void task1() {
@@ -15,4 +19,28 @@ public class Main {
             System.out.println("он не достиг совершеннолетия, нужно немного подождать");
         }
     }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int temperature = 10;
+        System.out.print("На улице " + temperature + " градусов, ");
+
+        if(temperature <= 5) {
+            System.out.println("нужно надеть шапку");
+        } else {
+            System.out.println("можно идти без шапки");
+        }
+    }
+
+    /*public static void task3() {
+        System.out.println("Задача 2");
+        int age = 17;
+        System.out.print("Если возраст члеовека равен " + age + " лет, то ");
+
+        if(age>=18) {
+            System.out.println("он совершеннолетний");
+        } else {
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }*/
 }
